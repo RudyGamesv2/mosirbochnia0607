640bd6c0ce52e77b2d693990dc9172128fe3eae0
RudyGamesv2 <94765507+RudyGamesv2@users.noreply.github.com>
2022-07-30 10:40:30 +0200

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,9 @@
+$(".hamburger").click(function(){
+    $(this).toggleClass("active")
+    $(".navigation-menu").toggleClass("active");
+});
+
+$(".navigation-item").click(function(){
+    $(".hamburger").toggleClass("active")
+    $(".navigation-menu").toggleClass("active");
+})
