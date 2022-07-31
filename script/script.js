b4d9d14b9968482b31ab61f469ea0206df0dffc9
RudyGamesv2 <94765507+RudyGamesv2@users.noreply.github.com>
2022-07-31 13:36:34 +0200

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -17,7 +17,6 @@ const cookies = document.querySelector('.CybotMultilevel.CybotCloseButtonEnabled
 $(".cookies-info-on").click(function(){
     $("#CookiebotWidget").toggleClass("CookiebotWidget-open")
     $(".CybotMultilevel .CybotCloseButtonEnabled").toggleClass("CybotMultilevel CybotCloseButtonEnabled CybotCookiebotDialogActive")
-    document.getElementById("CybotCookiebotDialog").setAttribute("style","opacity:0;");
 })
 
 function changeStyle(){
